718b0f82bdcaf352ba9518f4480d53665486690e
snaphuman <fhernandezn@gmail.com>
2019-04-13 19:03:49 -0500

    [dapp] add AE local devnet configuration

---
+++ assets/js/settings.js
@@ -0,0 +1,11 @@
+
+var settings = {
+    contractAddress: 'ct_2QPLSMyHi9wJEdGcpLJoYs4ptGZTkejY5qicrLKWKchTRNr5Uy',
+    account: {
+        pub: 'ak_zPoY7cSHy2wBKFsdWJGXM7LnSjVt6cn1TWBDdRBUMC7Tur2NQ',
+        priv: '36595b50bf097cd19423c40ee66b117ed15fc5ec03d8676796bdf32bc8fe367d82517293a0f82362eb4f93d0de77af5724fba64cbcf55542328bc173dbe13d33'
+    },
+    url: 'https://sdk-testnet.aepps.com',
+    internalUrl: 'https://sdk-testnet.aepps.com/internal/',
+    networkId: 'testnet'
+};
