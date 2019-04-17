261b73feb071d9feec2992c4144a75bdf7d947c5
snaphuman <fhernandezn@gmail.com>
2019-04-17 08:33:39 -0500

    [dapp] add commented ae_devnet settings

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,11 +1,22 @@
-
+/**
+   var settings = {
+    contractAddress: 'ct_HVb6d4kirgqzY1rShmzRTRwukcsXobjHcpLVD2EggoHmn6wt2',
+    account: {
+        pub: 'ak_23p6pT7bajYMJRbnJ5BsbFUuYGX2PBoZAiiYcsrRHZ1BUY2zSF',
+        priv: 'e15908673cda8a171ea31333538437460d9ca1d8ba2e61c31a9a3d01a8158c398a14cd12266e480f85cc1dc3239ed5cfa99f3d6955082446bebfe961449dc48b'
+    },
+    url: 'http://127.0.0.1:3001/',
+    internalUrl: 'http://127.0.0.1:3001/internal/',
+    networkId: 'ae_devnet'
+};
+*/
 var settings = {
-    contractAddress: 'ct_2dTvvytiQisXWzGmsNYEqqffyKAnZguLq3MvDoWYEBSaSLSSeD',
+    contractAddress: 'ct_2jLe9o6t3XJDV2iwAChQUNW4obT6YjnFawXctFqffNbo1YfC3r',
     account: {
         pub: 'ak_6A2vcm1Sz6aqJezkLCssUXcyZTX7X8D5UwbuS2fRJr9KkYpRU',
         priv: 'a7a695f999b1872acb13d5b63a830a8ee060ba688a478a08c6e65dfad8a01cd70bb4ed7927f97b51e1bcb5e1340d12335b2a2b12c8bc5221d63c4bcb39d41e61'
     },
     url: 'https://sdk-testnet.aepps.com',
     internalUrl: 'https://sdk-testnet.aepps.com',
-    networkId: 'ae_devnet'
+    networkId: 'ae_uat'
 };
